7be6b80570fe7692c64ce382cd7f84b541fa27c5
thomasjones <tgjones@byu.edu>
2025-03-13 12:57:26 -0600

    added cookies and bycrpyt dependencies

--- service/index.js
+++ service/index.js
@@ -1,7 +1,17 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
 const express = require('express');
 const app = express();
 
+const authCookieName = 'token';
+
+let users = [];
+
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
