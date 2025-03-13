b996634043906eaec26684944801ddf42b03f9b3
thomasjones <tgjones@byu.edu>
2025-03-13 13:33:59 -0600

    added simon createUser code to startup service index.js, will need to modify

--- service/index.js
+++ service/index.js
@@ -2,20 +2,107 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
-const express = require('express');
 const app = express();
 
 const authCookieName = 'token';
 
+// The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = [];
 
+// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+// JSON body parsing using built-in middleware
 app.use(express.json());
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Simon service' });
-});
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await createUser(req.body.email, req.body.password);
+  
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+    }
+  });
+
+  // GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+  
+  // DeleteAuth logout a user
+  apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+  });
+
+  // Need to add createTeam/setTeam(createsGame) endpoint
+
+  //update game endpoint?
+
+  //get randomword endpoint?
+
+  //clear endpoint?
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+  });
+
+
+//Helper functions/middleware wil go here
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    users.push(user);
+  
+    return user;
+  }
+  
+  async function findUser(field, value) {
+    if (!value) return null;
+  
+    return users.find((u) => u[field] === value);
+  }
+
+  // setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
