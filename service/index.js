6df92f19654f4440237a9ae60ce52079aa86ad40
thomasjones <tgjones@byu.edu>
2025-03-25 18:56:51 -0600

    now adds user to mono db

--- service/index.js
+++ service/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const { runGame, pickDescriber, startRound, compareWords, getRandomWord, resetGame } = require('./gameHelper');
-
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
@@ -273,7 +273,7 @@ async function createUser(username, email, password) {
       password: passwordHash,
       token: uuid.v4(),
     };
-    users.push(user);
+    await DB.addUser(user);
   
     return user;
   }
