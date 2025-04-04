e4d58833ff0ee40f4e991c256584deb4b26d1aff
thomasjones <tgjones@byu.edu>
2025-04-04 00:04:16 -0600

    edited config so that websocket also uses localhost:3000

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': 'http://localhost:3000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
     },
   },
 });
